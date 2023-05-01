2deb770be00f4fc7b0ff40fc88813ef9480909f8
noahbetten <duc@bluespacecreative.com>
2023-05-01 12:24:44 -0500

    updating styling on the products carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-let modal = document.getElementById("modal");
-let modalOpens = document.getElementsByClassName("modal-open");
-let modalCloses = document.getElementsByClassName("modal-close");
+let modal = document.getElementById("modal")
+let modalOpens = document.getElementsByClassName("modal-open")
+let modalCloses = document.getElementsByClassName("modal-close")
 for (let modalOpen of modalOpens) {
-	modalOpen.onclick = function () {
-		modal.classList.add("is-modal-open");
-	};
+  modalOpen.onclick = function () {
+    const modalName = modalOpen.dataset.modal
+    console.log(modalName)
+    document.getElementById(modalName).classList.add("is-modal-open")
+  }
 }
 for (let modalClose of modalCloses) {
-	modalClose.onclick = function () {
-		modal.classList.remove("is-modal-open");
-	};
+  modalClose.onclick = function () {
+    modalClose.parentElement.parentElement.parentElement.classList.remove("is-modal-open")
+  }
 }
-
-
