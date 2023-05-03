baa0520e540833ed56615eb8dc92165ed9579de2
noahbetten <duc@bluespacecreative.com>
2023-05-03 10:38:53 -0500

    creating js method for populating the products

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,54 @@
-let modal = document.getElementById("modal")
+import { createProduct } from "./ProductCard.js"
+
+const modals = {
+  "modal-1": [
+    {
+      logoAlt: "Polly Cupped Flighting",
+      imgSrc: "https://www.lundellplastics.com/images/product-lineup-Poly-Cupped-Flighting.png",
+      logoSrc: "/assets/images/pollyCuppedFlighting.svg",
+      productPageUrl: "https://www.lundellplastics.com/planting-products/poly-cupped-flighting.asp",
+      productTitle: "Polly Cupped Flighting",
+      desc: "What happens when you combine the longevity of Lundell's poly technology with trusted university testing? You get Poly Cupped Flighting, a tough industry giant that handles your crop with care.",
+    },
+  ],
+  "modal-2": [
+    {
+      logoAlt: "Polly Cupped Flighting",
+      imgSrc: "https://www.lundellplastics.com/images/product-lineup-Poly-Cupped-Flighting.png",
+      logoSrc: "/assets/images/pollyCuppedFlighting.svg",
+      productPageUrl: "https://www.lundellplastics.com/planting-products/poly-cupped-flighting.asp",
+      productTitle: "Polly Cupped Flighting",
+      desc: "What happens when you combine the longevity of Lundell's poly technology with trusted university testing? You get Poly Cupped Flighting, a tough industry giant that handles your crop with care.",
+    },
+  ],
+  "modal-3": [
+    {
+      logoAlt: "Polly Cupped Flighting",
+      imgSrc: "https://www.lundellplastics.com/images/product-lineup-Poly-Cupped-Flighting.png",
+      logoSrc: "/assets/images/pollyCuppedFlighting.svg",
+      productPageUrl: "https://www.lundellplastics.com/planting-products/poly-cupped-flighting.asp",
+      productTitle: "Polly Cupped Flighting",
+      desc: "What happens when you combine the longevity of Lundell's poly technology with trusted university testing? You get Poly Cupped Flighting, a tough industry giant that handles your crop with care.",
+    },
+  ],
+}
+
 let modalOpens = document.getElementsByClassName("modal-open")
 let modalCloses = document.getElementsByClassName("modal-close")
 for (let modalOpen of modalOpens) {
   modalOpen.onclick = function () {
     const modalName = modalOpen.dataset.modal
-    console.log(modalName)
-    document.getElementById(modalName).classList.add("is-modal-open")
+    const modal = document.getElementById(modalName)
+    modal.classList.add("is-modal-open")
+    var elem = modal.querySelector(".js-flickity")
+
+    console.log(elem)
+    var flkty = new Flickity(elem, {
+      // options
+      cellAlign: "left",
+      contain: true,
+    })
+    modal.querySelector(".js-flickity").innerHTML = modals[modalName].map(createProduct).join("")
   }
 }
 for (let modalClose of modalCloses) {
